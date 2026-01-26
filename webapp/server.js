129aafe35263ae70ee4b43b40d75273a3dfd67ef
Kevin <mahendra.rainafa@gmail.com>
2026-01-26 10:14:13 +0700

    node.js version

--- webapp/server.js
+++ webapp/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
+const cors = require("cors"); // 1. Tambahkan ini
 const app = express();
+
+app.use(cors()); // 2. Izinkan semua domain (Vercel) mengakses Node.js kamu
 
 app.use(express.json());
 
